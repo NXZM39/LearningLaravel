291de7c4683e0494ae3b115ce5a9609b532ab51a
Niezam <muhammadniezam29@gmail.com>
2021-10-08 03:25:20 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" 
+    integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" 
+    crossorigin="anonymous">
     <title>Livewire</title>
+    
 
-    @livewireStyles
+    <livewire:styles />
+
 </head>
 <body>
-    @livewire('counter')
-    @livewireScripts
+   
+    <livewire:comments />
+    <livewire:scripts />
 </body>
 </html>
